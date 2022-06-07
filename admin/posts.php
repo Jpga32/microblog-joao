43e9a45b895c55ccabdf9f25b0b50cf301111540
João Pedro <pedroganacim@gmail.com>
2022-06-07 16:54:40 -0300

    aatt

--- admin/posts.php
+++ admin/posts.php
@@ -1,53 +1,77 @@
-<?php 
-require "../inc/cabecalho-admin.php"; 
+<?php
+require "../inc/funcoes-posts.php";
+require "../inc/cabecalho-admin.php";
 
-?>      
-    
+$idUsuarioLogado = $_SESSION['id'];
+
+$tipoUsuarioLogado =  $_SESSION['tipo'];
+
+$posts = lerPosts($conexao, $idUsuarioLogado, $tipoUsuarioLogado);
+
+$quantidade = count($posts);
+
+
+?>
+
 <div class="row">
   <article class="col-12 bg-white rounded shadow my-1 py-4">
-    <h2 class="text-center">Posts <span class="badge badge-primary">X</span></h2>
+    <h2 class="text-center">Posts <span class="badge badge-primary"><?= $quantidade ?></span></h2>
     <p class="lead text-right">
       <a class="btn btn-primary" href="post-insere.php">Inserir novo post</a>
     </p>
-            
-    <div class="table-responsive"> 
+
+    <div class="table-responsive">
 
       <table class="table table-hover">
         <thead class="thead-light">
           <tr>
             <th>Título</th>
+
             <th>Data</th>
-            <th>Autor</th>
+
+            <?php if ($tipoUsuarioLogado == 'admin') { ?>
+
+              <th>Autor</th>
+
+            <?php } ?>
             <th colspan="2" class="text-center">Operações</th>
           </tr>
         </thead>
-      
+
         <tbody>
+          <?php foreach ($posts as $post) { ?>
+            <tr>
+              <td> <?= $post['titulo'] ?></td>
 
-          <tr>
-            <td> Título do post... </td>
-            <td> 21/12/2112 21:12 </td>
-            <td> Autor do post... </td>
-            <td class="text-center">
-              <a class="btn btn-warning btn-sm" 
-              href="post-atualiza.php">
+              <td> <?=  date_default_timezone_set('America/Sao_Paulo'); return  $post['data'] ?> </td>
+
+              <?php if ($tipoUsuarioLogado == 'admin') {  ?>
+
+                <td> <?= $post['autor'] ?> </td>
+
+              <?php } ?>
+              <td class="text-center">
+
+                <a class="btn btn-warning btn-sm" href="post-atualiza.php?id=<?= $post['id'] ?>">
                   Atualizar
-              </a>
-            </td>
-            <td class="text-center">
-              <a class="btn btn-danger btn-sm excluir"
-              href="post-exclui.php">
+                </a>
+              </td>
+              <td class="text-center">
+                <a class="btn btn-danger btn-sm excluir" href="post-exclui.php?id=<?= $post['id'] ?>">
                   Excluir
-              </a>
-            </td>
-          </tr>
+                </a>
 
-        </tbody>                
+              <?php } ?>
+
+              </td>
+            </tr>
+
+        </tbody>
       </table>
-      
+
     </div>
   </article>
 </div>
-     
 
-<?php require "../inc/rodape-admin.php"; ?> 
+
+<?php require "../inc/rodape-admin.php"; ?>
--- inc/funcoes-posts.php
+++ inc/funcoes-posts.php
@@ -2,24 +2,30 @@
 require "conecta.php";
 
 /* Usada em post-insere.php */
-function inserirPost(mysqli $conexao, string $titulo, string $texto, string $resumo, string $imagem, int $idUsuariologado)
-{
-    $sql = "INSERT INTO posts (titulo, texto, resumo, imagem, usuario_id)
-    VALUES ('$titulo','$texto','$resumo','$imagem',$idUsuariologado)";
-
+function inserirPost(mysqli $conexao, string $titulo, string $texto, string $resumo, string $imagem, int $idUsuarioLogado){
+    $sql = "INSERT INTO posts(titulo, texto, resumo, imagem, usuario_id) VALUES('$titulo', '$texto', '$resumo', '$imagem', $idUsuarioLogado)";
+    
     mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 } // fim inserirPost
 
 
 
 /* Usada em posts.php */
-function lerPosts(mysqli $conexao): array
-{
-    $sql = "";
+function lerPosts(mysqli $conexao, int $idUsuarioLogado, string $tipoUsuarioLogado):array {
+    // se o tipo de usuario for admin
+    if ($tipoUsuarioLogado == 'admin') {
+        // sql que traga TODOS os posts
+        $sql = "SELECT posts.id, posts.titulo, posts.data, usuarios.nome AS autor FROM posts INNER JOIN usuarios ON posts.usuario_id = usuarios.id ORDER BY data DESC";      
+    } else {
+        //Senão, sql que traga APENAS os posts do editor
+        $sql = "SELECT id,titulo, data From posts 
+              WHERE usuario_id = $idUsuarioLogado  ORDER BY data DESC";
+    }
+    
 
-    $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+    $resultado = mysqli_query($conexao,$sql) or die(mysqli_error($conexao));
     $posts = [];
-    while ($post = mysqli_fetch_assoc($resultado)) {
+    while($post = mysqli_fetch_assoc($resultado)){
         array_push($posts, $post);
     }
     return $posts;
@@ -27,32 +33,29 @@ function lerPosts(mysqli $conexao): array
 
 
 /* Usada em post-atualiza.php */
-function lerUmPost(mysqli $conexao): array
-{
+function lerUmPost(mysqli $conexao):array {    
     $sql = "";
 
-    $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
-    return mysqli_fetch_assoc($resultado);
+	$resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+    return mysqli_fetch_assoc($resultado); 
 } // fim lerUmPost
 
 
 
 /* Usada em post-atualiza.php */
-function atualizarPost(mysqli $conexao)
-{
+function atualizarPost(mysqli $conexao){
     $sql = "";
 
-    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));       
 } // fim atualizarPost
 
 
 
 /* Usada em post-exclui.php */
-function excluirPost(mysqli $conexao)
-{
+function excluirPost(mysqli $conexao){    
     $sql = "";
 
-    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+	mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 } // fim excluirPost
 
 
@@ -60,38 +63,27 @@ function excluirPost(mysqli $conexao)
 /* Funções utilitárias */
 
 /* Usada em post-insere.php e post-atualiza.php */
-function upload($arquivo)
-{
-    //Definindo tipos de imagens aceitos
-    $tiposValidos = [
-        'image/png',
-        'image/jpeg',
-        'image/gif',
-        'image/svg+xml'
-    ];
+function upload($arquivo){
+    // definir os tipos de imagens aceitas
+    $tiposValidos = ['image/png', 'image/jpeg', 'image/gif', 'image/svg+xml'];
 
-    // Verificar se o arquivo enviado nao e um dos aceitos
+    // verificando se o arquivo enviado NÃO É um dos aceitos
+    if(!in_array($arquivo['type'], $tiposValidos)){
+        die("<script> alert('Formato inválido!'); history.back(); </script>");
+    }
 
-    if (!in_array($arquivo['type'], $tiposValidos)) {
-        die("<script>alert('Formato é invalido  ¯\_(ツ)_/¯'); history.back(); </script>");
-    };
+    // acessando apenas o nome do arquivo
+    $nome = $arquivo['name']; //$_FILES['arquivo']['name'];
 
-    //Acessando apenas nome do arquivo
-
-    $nome = $arquivo['name']; // $_FILES ['arquivo'],['name'];
-
-    //Acessando dados de acesso temporário ao aquivo
+    // acessando dados de acesso temporario ao arquivo
     $temporario = $arquivo['tmp_name'];
-    
-    // Pasta de destino do arquivo que está sendo enviado
+
+    // pasta de destino do arquivo que está sendo enviado
     $destino = "../imagens/$nome";
 
-    /* Se o processo de envio temporario para destino for feito com sucesso, então 
-    a função retorna verdadeiro (indicando o sucesso no processo) */
-    
-    if(move_uploaded_file($temporario,$destino)){
-        return true ;
-
+    // se o processo de envio temporario para destino for feito com sucesso, então a função retorna verdadeiro (indicando sucesso no processo)
+    if(move_uploaded_file($temporario, $destino)){
+        return true;
     }
 
 } // fim upload
@@ -99,8 +91,8 @@ function upload($arquivo)
 
 
 /* Usada em posts.php e páginas da área pública */
-function formataData()
-{
+function formataData(){ 
+    
 } // fim formataData
 
 
@@ -108,40 +100,37 @@ function formataData()
 /*** Funções para a área PÚBLICA do site ***/
 
 /* Usada em index.php */
-function lerTodosOsPosts(mysqli $conexao): array
-{
+function lerTodosOsPosts(mysqli $conexao):array {
     $sql = "";
-
+    
     $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
     $posts = [];
-    while ($post = mysqli_fetch_assoc($resultado)) {
+    while( $post = mysqli_fetch_assoc($resultado) ){
         array_push($posts, $post);
     }
-    return $posts;
+    return $posts; 
 } // fim lerTodosOsPosts
 
 
 
 /* Usada em post-detalhe.php */
-function lerDetalhes(mysqli $conexao): array
-{
+function lerDetalhes(mysqli $conexao):array {    
     $sql = "";
 
     $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
-    return mysqli_fetch_assoc($resultado);
+    return mysqli_fetch_assoc($resultado); 
 } // fim lerDetalhes
 
 
 
 /* Usada em search.php */
-function busca($conexao): array
-{
+function busca($conexao):array {
     $sql = "";
-
+        
     $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
     $posts = [];
-    while ($post = mysqli_fetch_assoc($resultado)) {
+    while( $post = mysqli_fetch_assoc($resultado) ){
         array_push($posts, $post);
     }
-    return $posts;
+    return $posts; 
 } // fim busca
